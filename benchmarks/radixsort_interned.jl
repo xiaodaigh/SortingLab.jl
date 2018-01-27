ed902da578257d6383974e0831c643804ad99a31
susabi <zhuojia.dai@gmail.com>
2018-01-27 22:27:25 +1100

    benchmarks for interned string sorting

--- benchmarks/radixsort_interned.jl
+++ benchmarks/radixsort_interned.jl
@@ -1,71 +1,79 @@
 using InternedStrings, SortingLab, DataFrames, StatsBase, BenchmarkTools
-N = 10_000_000
+N = 20_000_000
 K = 100
-
-srand(1);
-@time samplespace = InternedString.("id".*dec.(1:N÷K, 10));
-
-# this is not the winner
-# function make_svec(N, samplespace)
-#     @time vss = rand(value.(samplespace), N)
-#     @time InternedString.(vss, true)
-# end
-
-@time svec = rand(samplespace, N)
-
-if false
-    # show that it's working
-    tic()
-    @time aaa = radixsort(svec);
-    2+2
-    toc()
-    # @time aaa1 = radixsort1(svec);
-    issorted(aaa)
-end
-
-tic()
-@time aaa = radixsort(svec);
-2+2
-toc()
-# @time aaa1 = radixsort1(svec);
-issorted(aaa)
-
-# run it 5 times manually to get the results
-# if you use @btime then it's likely to report the one with GC so not accurate
-interned_radixsort_timing = @belapsed radixsort($svec);
-
-@time radixsort(svec);
-
-srand(1);
-@time samplespace1 = "id".*dec.(1:N÷K, 10)
-@time svec1 = rand(samplespace1,N);
-
-tic()
-radixsort1_timing = @belapsed radixsort($svec1);
-2+2
-toc()
-
-using RCall
-
-r_timing = R"""
-memory.limit(40000)
-N=2e8; K=100
-set.seed(1)
-system.time(d1 <- sample(sprintf("id%010d",1:(N/K)), N, T))
-pt = proc.time()
-system.time(sort(d1, method="radix"))
-2+2
-#data.table::timetaken(pt)
-pt2 = proc.time()
-rm(d1); gc()
-pt2 - pt
-"""
-
+using RCall, CSV
 using Plots
 
-bar(
-    ["InternedStrings.jl sort", "Julia radix sort", "R radix sort"], 
-    [interned_radixsort_timing, radixsort1_timing, r_timing[3]],
-    title = "Interned Strings sort speed Julia vs R")
-savefig("Interned Strings sort speed Julia vs R.png")
+function compare_interned_string_sort(N, K)
+    srand(1);
+    @time samplespace = InternedString.("id".*dec.(1:N÷K, 10));
 
+    # this is not the winner
+    # function make_svec(N, samplespace)
+    #     @time vss = rand(value.(samplespace), N)
+    #     @time InternedString.(vss, true)
+    # end
+
+    @time svec = rand(samplespace, N);
+    # @benchmark radixsort($svec)
+
+
+    if false
+        # show that it's working
+        tic()
+        @time aaa = radixsort(svec);
+        2+2
+        toc()
+        # @time aaa1 = radixsort1(svec);
+        issorted(aaa)
+    end
+
+    # interned_radixsort_timing = @belapsed radixsort($svec);
+    interned_radixsort_timings = [@elapsed(radixsort(svec)) for i=1:5]
+    interned_radixsort_timing = mean(interned_radixsort_timings)
+
+    srand(1);
+    @time samplespace1 = "id".*dec.(1:N÷K, 10);
+    @time svec1 = rand(samplespace1,N);
+    # @benchmark radixsort($svec1)
+
+    # radixsort1_timing = @belapsed radixsort($svec1);
+    radixsort1_timings = [@elapsed(radixsort(svec1)) for i=1:5]
+    radixsort1_timing = mean(radixsort1_timings)
+ 
+    r_timing = R"""
+    memory.limit(40000)
+    N=$N; K=$K
+    res = NULL
+    set.seed(1)
+    system.time(d1 <- sample(sprintf("id%010d",1:(N/K)), N, T))
+    for(i in 1:5) {
+        pt = proc.time()
+        system.time(sort(d1, method="radix"))
+        2+2
+        #data.table::timetaken(pt)
+        pt2 = proc.time()
+        gc()
+        res = c(res, (pt2 - pt)[3])
+    }
+    res
+    """
+
+    df = DataFrame(
+        contestant = ["InternedStrings.jl sort", "Julia radix sort", "R radix sort"],
+        timing = [interned_radixsort_timing, radixsort1_timing, mean(r_timing)])
+
+    CSV.write("benchmarks/interned_string_results/Interned Strings sort speed Julia vs R $(N÷1_000_000)m.csv", df)
+
+    bar(
+        df[:contestant], 
+        df[:timing],
+        title = "Interned Strings sort speed Julia vs R $(N÷1_000_000)m",
+        label = "seconds")
+    
+    savefig("benchmarks/interned_string_results/Interned Strings sort speed Julia vs R $(N÷1_000_000)m.png")
+end
+
+# compare_interned_string_sort(1000,10)
+
+@time compare_interned_string_sort.((1:10).*(10^7), 100)
