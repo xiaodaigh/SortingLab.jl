3c3068f5f6f09b7635429b033e5092090d9a6d52
ZJ Dai <zhuojia.dai@gmail.com>
2023-03-14 21:06:21 +1100

    fix: floats

--- src/uint_map.jl
+++ src/uint_map.jl
@@ -1,8 +1,15 @@
-import Base.Sort:  uint_map
+import Base.Sort: uint_map
 export uint_map
 
 uint_map(x) = uint_map(x, Base.Forward)
 
-uint_map(::Missing) = UInt(2)^8sizeof(UInt)-1
+uint_map(x::Float16, ::Base.ReverseOrdering) = ~reinterpret(Int16, x)
+uint_map(x::Float16, ::Base.ForwardOrdering) = reinterpret(Int16, x)
+uint_map(x::Float32, ::Base.ReverseOrdering) = ~reinterpret(Int32, x)
+uint_map(x::Float32, ::Base.ForwardOrdering) = reinterpret(Int32, x)
+uint_map(x::Float64, ::Base.ReverseOrdering) = ~reinterpret(Int64, x)
+uint_map(x::Float64, ::Base.ForwardOrdering) = reinterpret(Int64, x)
 
-uint_map(::Missing, _) = UInt(2)^8sizeof(UInt)-1
+uint_map(::Missing) = UInt(2)^8sizeof(UInt) - 1
+
+uint_map(::Missing, _) = UInt(2)^8sizeof(UInt) - 1
